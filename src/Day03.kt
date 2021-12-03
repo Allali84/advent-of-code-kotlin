2fcbc2c9da86a1c686f6d3a0cd5f35f79b923462
Allali84 <allali.elkhalili@gmail.com>
2021-12-03 16:50:36 +0100

    Commit Day3 again so late

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,34 +1,78 @@
+import java.util.stream.IntStream
+
 fun main() {
     fun part1(input: List<String>): Int {
-        val forwards = input.filter { it.split(" ")[0] == "forward" }.sumOf { it.split(" ")[1].toInt() }
-        val ups = input.filter { it.split(" ")[0] == "up" }.sumOf { it.split(" ")[1].toInt() }
-        val downs = input.filter { it.split(" ")[0] == "down" }.sumOf { it.split(" ")[1].toInt() }
-        return forwards * (downs - ups)
-    }
 
-    var aim = 0
-    var forwards = 0
-    var depths = 0
+        var gamma = ""
+        var epsilon = ""
+        val countersList = mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        val size = input.size / 2
 
-    fun part2(input: List<String>): Int {
-        input.forEach {
-            when(it.split(" ")[0]) {
-                "down" -> aim += it.split(" ")[1].toInt()
-                "up" -> aim -= it.split(" ")[1].toInt()
-                "forward" -> {
-                    depths += aim * it.split(" ")[1].toInt()
-                    forwards += it.split(" ")[1].toInt()
+        input.forEach { number ->
+            number.forEachIndexed { i, it ->
+                if (it == '1') {
+                    countersList[i]++
                 }
             }
         }
-        return forwards * depths
+
+        countersList.forEach{
+            if (it < size) {
+                gamma += "0"
+                epsilon += "1"
+            } else {
+                gamma += "1"
+                epsilon += "0"
+            }
+        }
+
+        return Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2)
+    }
+
+    fun evaluate(list1: List<String>, list2: List<String>, forOxygen: Boolean):Boolean {
+        return if (forOxygen) list1.size > list2.size else list1.size < list2.size
+    }
+
+    fun recursive(input: List<String>, index: Int, forOxygen: Boolean = true ): List<String> {
+        val list1 = input.toMutableList()
+        if (list1.size == 2) {
+            if (forOxygen) {
+                if (list1[0][index] == '1') {
+                    list1.removeAt(1)
+                } else {
+                    list1.removeAt(0)
+                }
+            } else {
+                if (list1[0][index] == '0') {
+                    list1.removeAt(1)
+                } else {
+                    list1.removeAt(0)
+                }
+            }
+            return list1
+        }
+        val list2 = input.toMutableList()
+        input.forEach { number ->
+            if (number[index] == '1') {
+                list1.remove(number)
+            } else {
+                list2.remove(number)
+            }
+        }
+        return if (evaluate(list1, list2, forOxygen)) recursive(list1, index + 1, forOxygen) else recursive(list2, index + 1, forOxygen)
+    }
+
+    fun part2(input: List<String>): Int {
+        val oxygen = Integer.parseInt(recursive(input, 0)[0], 2)
+        val co2 = Integer.parseInt(recursive(input, 0, false)[0], 2)
+        return oxygen * co2
     }
 
     // test if implementation meets criteria from the description, like:
     //val testInput = readInput("Day01_test")
     //check(part1(testInput) == 1)
 
-    val input = readInput("Day02")
+    val input = readInput("Day03")
     println(part1(input))
     println(part2(input))
 }
